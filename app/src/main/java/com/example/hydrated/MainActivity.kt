efe21434220c35857e14bc1dfb6cf2bdc380ab3a
Saien <ST10133334@vcconnect.edu.za>
2025-09-23 13:54:51 +0200

    Initial

---
+++ app/src/main/java/com/example/hydrated/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.hydrated
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonStart : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+//        buttonStart = findViewById(R.id.btnStart)
+//
+//        buttonStart.setOnClickListener {
+//            val startIntent : Intent
+//            startIntent = Intent(this@MainActivity, LoginActivity::class.java)
+//            startActivity(startIntent)
+//        }
+    }
+}
